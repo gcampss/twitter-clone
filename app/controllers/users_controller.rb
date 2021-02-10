6c67715c97bca4f9d813e5d8985e67d8aa6cae8f
Gabriel Campista <gcampss@gmail.com>
2021-02-10 20:08:36 +0000

    add date to posts desc order post ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # - > todos os posts
   end
 
   def edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  default_scope {order(created_at: :desc)}
+
   validates :content, presence: true
   validates :content, length: { maximum: 280 }
 
