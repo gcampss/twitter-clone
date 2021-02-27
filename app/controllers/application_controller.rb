e76f8cd352b004c717613401af159fbc6cf6baa2
Gabriel Campista <gcampss@gmail.com>
2021-02-27 17:21:21 +0000

    adding seeds fixing sign up follow control nok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name nickname bio avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname bio avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name nickname bio photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname bio photo])
   end
 end
