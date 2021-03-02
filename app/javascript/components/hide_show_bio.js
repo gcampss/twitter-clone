e477003d41a9d9d8e4585967a2ed7a59d4dd0674
Rodrigo Ruas <rodrigoruas2@gmail.com>
2021-03-02 12:05:58 +0000

    Ro JS Examples

---
+++ app/javascript/components/hide_show_bio.js
@@ -0,0 +1,35 @@
+// Tudo fica numa função (metodo)
+
+const buttonHide = document.getElementById("hide-button")
+const buttonShow = document.getElementById("show-button")
+
+const handleBio = () => {
+  buttonHide.addEventListener('click', (event) => {
+    event.preventDefault();
+    hideBio();
+  })
+
+  buttonShow.addEventListener('click', (event) => {
+    event.preventDefault();
+    showBio();
+  })
+}
+
+const hideBio = () => {
+  const bio = document.getElementById("user-bio")
+  bio.classList.toggle("d-none")
+  // esconder o botao hide
+  // mostrar o botao show
+
+}
+
+const showBio = () => {
+  const bio = document.getElementById("user-bio")
+  bio.classList.toggle("d-none")
+  // esconder o botao show
+  // mostrar o botao hide
+}
+
+// exporta o const
+
+export { handleBio }
